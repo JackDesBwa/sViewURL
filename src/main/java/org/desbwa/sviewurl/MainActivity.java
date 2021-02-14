a098c89db9006656197735c79c245ab1add3f186
JackDesBwa <jackdesbwa@gmail.com>
2021-02-14 22:08:01 +0100

    Handle empty clipboard

--- src/main/java/org/desbwa/sviewurl/MainActivity.java
+++ src/main/java/org/desbwa/sviewurl/MainActivity.java
@@ -2,6 +2,7 @@ package org.desbwa.sviewurl;
 
 import android.app.Activity;
 import android.content.ClipboardManager;
+import android.content.ClipData;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -34,32 +35,47 @@ public class MainActivity extends Activity {
 			source = "Shared URL";
 		} else {
 			ClipboardManager clip = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
-			url = clip.getPrimaryClip().getItemAt(0).coerceToText(getApplicationContext()).toString();
+			if (clip != null) {
+				ClipData data = clip.getPrimaryClip();
+				if (data != null && data.getItemCount() > 0) {
+					url = data.getItemAt(0).coerceToText(getApplicationContext()).toString();
+				} else {
+					url = "";
+				}
+			} else {
+				url = "";
+			}
 			source = "Clipboard";
 		}
 
-		LinearLayout l = new LinearLayout(this);
-		l.setOrientation(LinearLayout.VERTICAL);
-		setContentView(l);
+		if (url != "") {
+			LinearLayout l = new LinearLayout(this);
+			l.setOrientation(LinearLayout.VERTICAL);
+			setContentView(l);
 
-		Button buttonImage = new Button(this);
-		buttonImage.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT, 0.5f));
-		buttonImage.setText(source + "\n↓\nsView Image");
-		buttonImage.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				open(url, false);
-			}
-		});
-		l.addView(buttonImage);
+			Button buttonImage = new Button(this);
+			buttonImage.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT, 0.5f));
+			buttonImage.setText(source + "\n↓\nsView Image");
+			buttonImage.setOnClickListener(new View.OnClickListener() {
+				public void onClick(View v) {
+					open(url, false);
+				}
+			});
+			l.addView(buttonImage);
 
-		Button buttonVideo = new Button(this);
-		buttonVideo.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT, 0.5f));
-		buttonVideo.setText(source + "\n↓\nsView Video");
-		buttonVideo.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				open(url, true);
-			}
-		});
-		l.addView(buttonVideo);
+			Button buttonVideo = new Button(this);
+			buttonVideo.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT, 0.5f));
+			buttonVideo.setText(source + "\n↓\nsView Video");
+			buttonVideo.setOnClickListener(new View.OnClickListener() {
+				public void onClick(View v) {
+					open(url, true);
+				}
+			});
+			l.addView(buttonVideo);
+		} else {
+			TextView label = new TextView(this);
+			label.setText("To use this app, add an URL in your clipboard and come back, or share an URL directly with the app.");
+			setContentView(label);
+		}
 	}
 }
